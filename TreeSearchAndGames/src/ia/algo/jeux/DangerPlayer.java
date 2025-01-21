08eef2ab3c8653b4aa3e9a0270d6a47a45ccaead
Jarod Toussaint <jarod.toussaint8@etu.univ-lorraine.fr>
2025-01-21 08:29:50 +0100

    [DOC] Javadoc classe DangerPlayer

--- TreeSearchAndGames/src/ia/algo/jeux/DangerPlayer.java
+++ TreeSearchAndGames/src/ia/algo/jeux/DangerPlayer.java
@@ -6,6 +6,9 @@ import ia.framework.jeux.Game;
 import ia.framework.jeux.GameState;
 import ia.framework.jeux.Player;
 
+/**
+ * Joueur qui joue en fonction du danger que représente un coup adversaire
+ */
 public class DangerPlayer extends Player {
 
     /**
@@ -25,17 +28,21 @@ public class DangerPlayer extends Player {
         for (Action coup : game.getActions(state)) {
             GameState stateSuivant = (GameState) game.doAction(state, coup);
 
+             //Si le coup est gagnant, on le joue
             if (stateSuivant.isFinalState()){
                 return coup;
             }
 
             for (Action coupAdverse : game.getActions(stateSuivant)) {
                 GameState stateAdverse = (GameState) game.doAction(stateSuivant, coupAdverse);
+                // Si le coup adverse est gagnant, on le bloque
                 if (game.endOfGame(stateAdverse) && stateAdverse.getGameValue() != 0) {
                     return coupAdverse;
                 }
             }
 
+            // Pour le joueur 1, on joue le coup qui augmente le plus le danger
+            // Pour le joueur 2, on joue le coup qui diminue le plus le danger
             double danger = stateSuivant.getGameValue();
             if (this.player == PLAYER1 && danger > meilleurDanger || this.player == PLAYER2 && danger < meilleurDanger) {
                 meilleurDanger = danger;
